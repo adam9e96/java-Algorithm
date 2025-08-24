72be284a451756bbd99463c12cbc057443bd8fa6
adam9e96 <adam9e96@gmail.com>
2025-08-25 00:44:45 +0900

    update

--- src/baekjoon/kotlin/_1008.kt
+++ src/baekjoon/kotlin/_1008.kt
@@ -1,15 +1,15 @@
 package baekjoon.kotlin
 
 /*
-  10998 번 문제
-  AxB
-  https://www.acmicpc.net/problem/10998
+  1008 번 문제
+  A/B
+  https://www.acmicpc.net/problem/1008
 */
 fun main() {
 //    val num1: Int = readLine()!!.toInt() // 입력받아 정수로 변환
 //    val num2: Int = readLine()!!.toInt()
 //    println(num1 * num2)
 
-    val number = readLine()!!.split(" ")
-    println(number[0].toInt() * number[1].toInt())
+    val number: List<String> = readln().split(" ")
+    println(number[0].toDouble() / number[1].toDouble())
 }
